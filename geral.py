4b1c7fa98642ac719bfc8701453713143903328e
Makley <tibolamakley1@gmail.com>
2020-12-01 17:29:38 -0300

    docs substituidos e implmentado metodos, remove_repetidas, remove_numeros_extenso, remove_numeros e trocado de local em que alguns metodos eram chamados

--- geral.py
+++ geral.py
@@ -1,14 +1,5 @@
 import glob, os, time, codecs, sys
 
-stopwords = []
-nome_arq = 'stopwords.txt'
-
-arq = codecs.open(nome_arq, "r", "UTF-8")
-linhas = arq.readlines()
-for linha in linhas:    
-    stopwords.append(linha.replace('\n', '').strip().lower())
-arq.close()
-
 def opcaoInvalida():
     print("========================================================")
     print("                   opção invalida!")
@@ -26,13 +17,12 @@ def lendoarquivo(nomeArquivo):
 
     # print(docNormal)
     return docNormal
-    
 
 def lendoarquivos():
     for arq in glob.glob("docs/*.txt"):
         print("[{}]".format(arq))
         docTemporario = ''
-    
+
         # Abrir arquivo
         f = codecs.open(arq, "r", "UTF-8")
         linhas = f.readlines()
@@ -60,7 +50,7 @@ def remove_stopwords(lista):
     nova_lista = []
 
     for token in lista:
-        if token.lower() not in stopwords:
+        if token.lower() not in stopwords and token.lower() != '':
             nova_lista.append(token)
 
     return nova_lista
@@ -68,49 +58,87 @@ def remove_stopwords(lista):
 def tokenizacao(doc_inicial):
     return doc_inicial.split(' ')
 
+
 def substituir_especiais(token):
-        
     for i in range(len(token)):
         if (token[i] == 'á' or token[i] == 'ã' or token[i] == 'â' or token[i] == 'à' or token[i] == 'ä'):
             token = token.replace(token[i], 'a')
-            print("a")
         elif (token[i] == 'ç'):
             token = token.replace(token[i], 'c')
-            print("c")
         elif (token[i] == 'é' or token[i] == 'ê' or i == 'ë'):
             token = token.replace(token[i], 'e')
-            print("e")
         elif (token[i] == 'í' or token[i] == 'ï'):
             token = token.replace(token[i], 'i')
-            print("i")
         elif (token[i] == 'ó' or token[i] == 'õ' or token[i] == 'ô' or token[i] == 'ö'):
             token = token.replace(token[i], 'o')
-            print("o")
         elif (token[i] == 'ú' or token[i] == 'ü'):
             token = token.replace(token[i], 'u')
-            print("u")
 
     return token
 
-def normalizacao(lista):
+def remove_numeros(token):
+    nova_lista = []
+    lista_numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    for i in range(len(token)):
+        if (token[i] not in lista_numeros):
+            nova_lista.append(token[i])
+
+    return nova_lista
+
+def remove_numeros_extenso(lista):
+    numeros_extenso = []
+    nova_lista = []
+    nome_arq = 'numeros.txt'
+
+    arq = codecs.open(nome_arq, "r", "UTF-8")
+    linhas = arq.readlines()
+    for linha in linhas:    
+        numeros_extenso.append(linha.replace('\n', '').strip().lower())
+    arq.close()
+
+    for token in lista:
+        if token.lower() not in numeros_extenso and token.lower() != '':
+            nova_lista.append(token)
+
+    return nova_lista
+    
+def normalizacao(linha):
     token = ''
     nova_lista = []
     pontuacoes = '!()[]{};:\'\"\,<>.?@#%^&*_~'
+
+    linhaFinal = substituir_especiais(linha)
+    linhaFinal = remove_numeros(linhaFinal)
     
-    for i in range(len(lista)):
-        for j in lista[i]:
+    for i in range(len(linhaFinal)):
+        for j in linhaFinal[i]:
             if (j in pontuacoes):
                 continue
             else:
-                # substituir_especiais(j)
                 token += j.lower()
         nova_lista.append(token)
+        retorno = "".join(nova_lista)
         token = ''
         
-    return nova_lista
-    
+    return retorno
 
-"""
+def remove_repetidas(lista):
+    conjunto = set(lista)
+    return conjunto
+
+# """"""""""""""""""""""remove stopwords""""""""""""""""""""""""""""""
+stopwords = []
+nome_arq = 'stopwords.txt'
+
+arq = codecs.open(nome_arq, "r", "UTF-8")
+linhas = arq.readlines()
+for linha in linhas:
+    linha = substituir_especiais(linha)
+    stopwords.append(linha.replace('\n', '').strip().lower())
+arq.close()
+# """"""""""""""""""""""remove stopwords""""""""""""""""""""""""""""""
+
 docTemporario = ''
 arq = "Docs/doc1.txt"
 f = codecs.open(arq, "r", "UTF-8")
@@ -118,36 +146,23 @@ linhas = f.readlines()
 
 for linha in linhas:
         # remove espaços em branco no inicio e fim de cada linha lida
+        linha = normalizacao(linha)
         docTemporario += linha.replace('\r\n',' ')
 f.close()
 
+docTemporario = docTemporario.replace("  "," ")
+docTemporario = docTemporario.replace("-"," ")
+docTemporario = docTemporario.replace("   "," ")
 docTemporario = tokenizacao(docTemporario)
 docTemporario = remove_stopwords(docTemporario)
-# docTemporario = normalizacao(docTemporario)
-
+docTemporario = remove_numeros_extenso(docTemporario)
+docTemporario = remove_repetidas(docTemporario)
 print(docTemporario)
 
-docTemporario = normalizacao(docTemporario)
+# ! primeiro  remove stopwords      ok
+# ! segundo   substituir_especiais  ok
+# ! terceiro  tokenizacao           ok
+# ! quarto    remove_stopwords      ok
+# ! quinto    normalizacao          ok
+# ! sexto    indexacao             
 
-print(docTemporario)
-
-# print(docTemporario)
-# print(normalizacao(tokenizacao(lendoarquivo(docTemporario))))
-"""
-
-# ! primeiro  tokenizacao           ok
-# ! segundo   remove_stopwords      ok
-# ! terceiro  normalizacao          ok
-# ! quarto    substituir_especiais 
-# ! quinto    indexacao
-
-carac = 'Com a vitória sobre o Uruguai, o Brasil chegou a 12 pontos e lidera as Eliminatórias da Copa do Mundo FIFA Catar 2022. Arthur vê a Seleção em boa situação na busca por uma vaga na Copa do Mundo, mas sabe que cada partida deve apresentar ainda mais dificuldade. "Nosso objetivo é esse (conquistar a classificação). Estamos trabalhando forte em busca do nosso objetivo. Sempre temos algo a melhorar. A gente não entrou pensando que estamos invictos, pois cada jogo é diferente e difícil. Estamos em um bom caminho, confiamos na comissão técnica e eles confiam na gente. Agora é seguir trabalhando e buscando essa classificação para o Mundial", concluiu.'
-
-
-# # substitui
-# ca = (carac.replace(carac[9], 'o'))
-# # volta a não estar substituido
-# print(carac)
-
-
-print(substituir_especiais(carac))
